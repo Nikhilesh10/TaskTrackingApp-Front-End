c396bbd4b95130f8e46b7485df961437765f6596
nikhilesh <nikhilesh5475@gmail.com>
2023-06-05 14:38:30 -0500

    Initial commit

--- redux/index.js
+++ redux/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login/Login';
+import Registration from './components/registration/Registration';
+import Home from './components/home/Home';
+import Mytasks from './components/mytasks/Mytasks';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [token, setToken] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    try {
+      const storedData = localStorage.getItem('userData');
+      const userData = JSON.parse(storedData);
+      const userToken = userData.token;
+
+      setToken(userToken);
+      setIsLoggedIn(!!token);
+    } catch (error) {
+      handleLogout();
+    }
+  }, []);
+
+  console.log("TOKEN at APP", token)
+
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    const storedData = localStorage.getItem('userData');
+      const userData = JSON.parse(storedData);
+      const userToken = userData.token;
+
+      setToken(userToken);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('userData');
+    setToken('');
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<Login handleLogin={handleLogin} isLoggedIn={isLoggedIn} />}
+          />
+          <Route path="/Registration" element={<Registration />} />
+          {isLoggedIn ? (
+            <>
+              <Route path="/Home/:id" element={<Home handleLogout={handleLogout} isLoggedIn={isLoggedIn} token={token} />} />
+              <Route path="/Mytasks/:id" element={<Mytasks handleLogout={handleLogout} isLoggedIn={isLoggedIn} token={token} />} />
+            </>
+          ) : (
+            "No Token"
+          )}
+        </Routes>
+      </main>
+    </BrowserRouter>
+
   );
 }
 
